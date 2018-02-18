98f10d26baed2a720f66c601f95520d25fe65d6e
c0debrain <codebrain@gmail.com>
2018-02-19 09:54:13 +1100

    add lookback percept

--- strategies/lookbackPercept.js
+++ strategies/lookbackPercept.js
@@ -6,6 +6,18 @@ const async = require ('async');
 const log = require('../core/log.js');
 const fs = require('fs');
 
+/* Candle information
+ { id: 103956,
+ start: moment("2018-02-04T00:00:00.000"),
+ open: 9080.49,
+ high: 9218.98,
+ low: 9022,
+ close: 9199.96,
+ vwp: 9097.252446880359,
+ volume: 802.5146890000001,
+ trades: 8086 }
+ */
+
 //performance data
 //4,3,1
 //2017-12-16 00:51
@@ -20,7 +32,9 @@ var method = {};
 
 // prepare everything our method needs
 method.init = function() {
-    this.weightFileName = "weights/lookbackPercept.json";
+    this.weightFileName = "weights/lookbackPercept-ethxrp.json";
+    //this.weightFileName = "weights/staticPercept-ethxrp-13-400-2018-02-13-07-35-3p.json";
+    //this.weightFileName = "weights/staticPercept.json";
     //this.weightFileName = "weights/staticPercept-11-200-338p.json";
     //this.weightFileName = "weights/staticPercept-3-400-392p.json";
 
@@ -36,150 +50,111 @@ method.init = function() {
 
     this.network=null;
     //NOTE: comment out to train and save
-    this.weights = this.readFromFile(this.weightFileName);
+    //this.weights = this.readFromFile(this.weightFileName);
 
-    log.info("**************************************");
+      log.info("**************************************");
     if(this.weights!=null) {
       log.info("***** Creating network from file *****");
       this.network = neataptic.Network.fromJSON(this.weights);
     } else {
       // preprate neural network
       log.info("*** Training network from scratch ****");
-      this.network = new neataptic.architect.Perceptron(4,3,1);
+      this.network = new neataptic.architect.Perceptron(12,3,1);
       //this.network = new neataptic.architect.LSTM(4,16,1);
     }
-      log.info("***************************************");
+      log.info("**************************************");
+
+
+    //use to train
+    this.lookbackIndex = 3;
+    this.lookbackData = [];
+    this.lookbackInput = [];
 
     this.trainingData = [];
     this.obj = {};
 
-    this.previousCandle = null;
-
-    this.lookbackData = [];
-    this.lookbackperiod = 2;
+    //use to activate
+    this.lookbackCheckData = [];
+    this.lookbackCheckInput = [];
 }
+
 
 // what happens on every new candle?
 method.update = function(candle) {
 
-    //this.obj['input'] = [candle.open/this.normalizer]; // divide with 20k, normalizing our input and output
-    //this.obj['output'] = [candle.close/this.normalizer];
-    if(this.lookbackData.length < this.lookbackperiod) {
-        this.lookbackData.push(candle);
+    //have weights no need to train
+    if(this.weights!=null || this.trainingData.length >= this.requiredHistory) {
         return;
     }
 
-    //we need to train
-    if(this.weights!=null) {
-      return;
+    //prepare input for training
+    if(this.lookbackData.length < this.lookbackIndex) {
+        this.lookbackData.push(candle);
+        return;
+    } else if (this.lookbackData.length > this.lookbackIndex ) {
+        this.lookbackData.shift();
     }
 
-    this.obj['input'] = [this.previousCandle.open,this.previousCandle.close,
-                        this.previousCandle.high,this.previousCandle.low]; // no normalizer
+    this.lookbackInput = this.getLookbackInput(this.lookbackData);
+
+    this.obj['input'] = this.lookbackInput;
     this.obj['output'] = [candle.close];
-
-
-
-    this.previousCandle = candle;
+    //remember this candel for next time
+    this.lookbackData.push(candle);
 
     // train the neural network
+    //log.info("pushing training data:");
+    //log.info(this.obj);
+
     this.trainingData.push(this.obj);
     //log.info("Pushing train data "+this.trainCounter++);
-
-    if(this.trainingData.length > this.requiredHistory) {
-      //log.info("trainDataSize size: "+this.trainingData.length);
-      //this.trainingData.shift();
-    }
 
     //log.info("update called: trainDataSize: "+this.trainingData.length);
 
     if(this.trainingData.length == this.requiredHistory && !this.weights != null) {
       log.info("Staring to train: "+this.trainingData.length);
-      log.info(this.obj['input']);
-      log.info(this.obj['output']);
+      //log.info(this.trainingData);
 
       //perceptron
       this.network.train(this.trainingData, {
           //dropout: 0.5,
           //clear: true,
-          shuffle:true,
           log: 0,
+          shuffle:true,
           iterations: 100000,
-          error: 0.00000000001,
+          error: 0.0000000001,
           rate: 0.03,
       });
-
+      log.info("Done training .. writing weights to file:");
       this.writeToFile();
   }
 
 }
 
 
-method.writeToFile = function() {
-  const exported = this.network.toJSON();
-  const content = JSON.stringify(exported);
-
-  fs.writeFile(this.weightFileName, content, function(err, data){
-      if (err) console.log(err);
-      console.log("Successfully Written to File.");
-  });
-}
-
-method.readFromFile = function(filePath) {
-    var data = fs.readFileSync(filePath,'utf8');
-    return JSON.parse(data);
-    /*
-
-    fs.stat(filePath, function(err, stat) {
-      if(err == null) {
-        fs.readFile(filePath, function read(err, data) {
-          if (err) {
-              log.error("ERROR reading weight file");
-              throw err;
-          }
-          log.info("Read weights from file");
-          log.info(data);
-          jsonData = JSON.parse(data);
-          log.info(jsonData);
-        });
-      } else if(err.code == 'ENOENT') {
-          // file does not exist
-          log.error("Network file dont exist")
-          //fs.writeFile('log.txt', 'Some log\n');
-      } else {
-          console.log('Some other error: ', err.code);
-      }
-  });
-  return jsonData;
-  */
-}
-
-
-method.log = function() {
-
-}
-
 // check is executed after the minimum history input
 method.check = function(candle) {
 
-    /* Candle information
-    { id: 103956,
-      start: moment("2018-02-04T00:00:00.000"),
-      open: 9080.49,
-      high: 9218.98,
-      low: 9022,
-      close: 9199.96,
-      vwp: 9097.252446880359,
-      volume: 802.5146890000001,
-      trades: 8086 }
-    */
+    if (this.trainingData.length < this.requiredHistory && this.weights==null) {
+        return;
+    }
 
+    this.lookbackCheckData.push(candle);
+    if(this.lookbackCheckData.length < this.lookbackIndex ) {
+        return;
+    } else if(this.lookbackCheckData.length > this.lookbackIndex ) {
+        this.lookbackCheckData.shift();
+    }
+
+    log.info("lookbackchack size: "+this.lookbackCheckData.length);
+    this.lookbackCheckInput = this.getLookbackInput(this.lookbackCheckData);
 
     //let's predict the next close price on the current close price;
     //var predicted_value = this.network.activate(candle.close/this.normalizer)*this.normalizer;
 
     //no normalizer
-    var predicted_value = this.network.activate([candle.open,candle.close,candle.high,candle.close]);
+    //var predicted_value = this.network.activate([candle.open,candle.close,candle.high,candle.low]);
+    var predicted_value = this.network.activate(this.lookbackCheckInput);
 
     // % change in current close and predicted close
     var percentage = ((predicted_value-candle.close)/candle.close)*100;
@@ -189,10 +164,12 @@ method.check = function(candle) {
     //log.info("Predicted value: "+predicted_value);
     //log.info("Percent: "+percentage);
 
+    log.info("Value: "+predicted_value+" percent: "+percentage);
+
     if(percentage > 1 && !this.open_order)
     {
         log.info("Buy: $"+candle.close+" expected percent: "+percentage);
-        this.price = candle.close;
+        //this.price = candle.close;
         this.open_order = true;
         return this.advice('long');
 
@@ -203,6 +180,40 @@ method.check = function(candle) {
     }
 
     return this.advice();
+}
+
+
+method.getLookbackInput = function(lookbackData) {
+    var lookbackInput = [];
+    for(var i=0;i<lookbackData.length;i++) {
+        lookbackInput.push(this.lookbackData[i].open);
+        lookbackInput.push(this.lookbackData[i].close);
+        lookbackInput.push(this.lookbackData[i].high);
+        lookbackInput.push(this.lookbackData[i].low);
+    }
+    return lookbackInput;
+}
+
+
+
+method.writeToFile = function() {
+    const exported = this.network.toJSON();
+    const content = JSON.stringify(exported);
+
+    fs.writeFile(this.weightFileName, content, function(err, data){
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+}
+
+method.readFromFile = function(filePath) {
+    var data = fs.readFileSync(filePath,'utf8');
+    return JSON.parse(data);
+}
+
+
+method.log = function() {
+
 }
 
 module.exports = method;
