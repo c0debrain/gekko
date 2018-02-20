29c068e4ff3b1048a89b3d73cb2b8891e2304464
c0debrain <codebrain@gmail.com>
2018-02-20 11:02:47 +1100

    greedy vegas 3

--- strategies/LabPercept.js
+++ strategies/LabPercept.js
@@ -32,8 +32,8 @@ var method = {};
 
 // prepare everything our method needs
 method.init = function() {
-    this.weightFileName = "weights/lookbackPercept-ethtrx-lab.json";
-    //this.weightFileName = "weights/lookbackPercept-ethtrx-1-1-3-2500-2-10-12p.json";
+    //this.weightFileName = "weights/lookbackPercept-ethtrx-lab.json";
+    this.weightFileName = "weights/lookbackPercept-ethtrx-1-1-3-2500-2-10-12p.json";
     //this.weightFileName = "weights/lookbackPercept-ethtrx-3-2400-2-10-10p.json";
     //this.weightFileName = "weights/lookbackPercept-ethxrp.json";
     //this.weightFileName = "weights/staticPercept-ethxrp-13-400-2018-02-13-07-35-3p.json";
@@ -53,7 +53,7 @@ method.init = function() {
 
     this.network=null;
     //NOTE: comment out to train and save
-    //this.weights = this.readFromFile(this.weightFileName);
+    this.weights = this.readFromFile(this.weightFileName);
 
 
     //use to train
