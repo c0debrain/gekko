b9cd2fcf08cf190be79abfc655128c21c634954d
c0debrain <codebrain@gmail.com>
2018-05-06 17:58:49 +1000

    update precept better detection

--- strategies/LabPerceptCl.js
+++ strategies/LabPerceptCl.js
@@ -318,7 +318,7 @@ method.check = function(candle) {
 
     } else if( this.open_order
                 && ( //predictPercent < 0 ||
-                        !isUptrendMoveAvg && profitPercent < this.pastProfitPercent
+                        !isUptrendMoveAgg && profitPercent < this.pastProfitPercent
                         || (predictPercent < -this.pricePredictPercent && profitPercent < this.pastProfitPercent)
                     )
             //&& ((profitPercent >= this.pricePredictPercent && profitPercent < this.pastProfitPercent))
