23f37e257f58b18a97a65f5841b557f980064174
c0debrain <codebrain@gmail.com>
2018-05-18 13:58:11 +1000

    refactor simple lstm

--- strategies/lstmLearn.js
+++ strategies/lstmLearn.js
@@ -4,25 +4,26 @@ const _ = require('lodash');
 const config = require ('../core/util.js').getConfig();
 const async = require ('async');
 const log = require('../core/log.js');
+const tu = require('../modules/tradeutil.js');
 
 // let's create our own method
 var method = {};
 
 // prepare everything our method needs
 method.init = function() {
+
+    tu.normalizer = 100;
+    tu.roundPoint = 7;
+
     this.name = '007';
     this.requiredHistory = config.tradingAdvisor.historySize;
     this.price = 0;
     this.open_order = false;
 
-    this.lookbackPeriod=3;
-    this.lookbackData = [];
-
     // preprate neural network
-    this.network = new neataptic.architect.LSTM(1,9,1);
+    this.network = new neataptic.architect.LSTM(1,6,1);
     //this.network = new neataptic.architect.NARX(1, 5, 1, 50, 50);
     this.trainingData = [];
-    this.obj = {};
     this.previous = null;
     this.trainResult = null;
     this.previousProfitPercent=0;
@@ -36,27 +37,29 @@ method.update = function(candle) {
     }
 
     var myObj = {};
-    myObj['input'] = [this.previous];
-    myObj['output'] = [candle.close];
+    myObj['input'] = [tu.getNorm(this.previous)];
+    myObj['output'] = [tu.getNorm(candle.close)];
 
     // train the neural network
     this.trainingData.push(myObj);
     this.previous = candle.close;
 
-    if(this.trainingData.length > 10) {
+    if(this.trainingData.length > this.requiredHistory) {
         this.trainingData.shift();
     }
 
     if(this.trainingData.length >= this.requiredHistory) {
 
         log.info("starting to train: "+this.trainingData.length);
-        log.info(this.trainingData);
+        //log.info(this.trainingData);
 
         this.trainResult = this.network.train(this.trainingData, {
-            log: 1000,
-            iterations: 3000,
-            error: 0.00000000003,
-            rate: 0.03
+            clear: true,
+            log: 10000,
+            iterations: 300000,
+            error: 0.00005,
+            rate: 0.000005,
+            momentum: 0.9
         });
         //console.log(this.trainResult);
     }
@@ -64,55 +67,37 @@ method.update = function(candle) {
 }
 
 
-
-method.log = function() {
-
-}
-
 // check is executed after the minimum history input
 method.check = function(candle) {
-
-    /* Candle information
-     { id: 103956,
-     start: moment("2018-02-04T00:00:00.000"),
-     open: 9080.49,
-     high: 9218.98,
-     low: 9022,
-     close: 9199.96,
-     vwp: 9097.252446880359,
-     volume: 802.5146890000001,
-     trades: 8086 }
-     */
 
     if(this.trainingData.length < this.requiredHistory) {
         return;
     }
 
-    console.log("tring to predict");
+    log.info("tring to predict");
+    log.info("****************");
 
     //let's predict the next close price on the current close price;
-    var predicted_value = this.network.activate(candle.close);
-
+    var normClose = tu.getNorm(candle.close);
+    var predicted_value = this.network.activate(normClose);
     // % change in current close and predicted close
-    var percentage = ((predicted_value-candle.close)/candle.close)*100;
-    var currentProfitPercent = this.getCurrentProfitPercent(candle.close);
+    var percentage = tu.getPercent(predicted_value,normClose);
+    var currentProfitPercent = tu.getPercent(normClose,this.price);
 
-    log.info("price: "+candle.close);
+    log.info("price: "+tu.getNorm(candle.close));
     log.info("predict: "+predicted_value);
     log.info("percentage: "+percentage);
     log.info("profit: "+currentProfitPercent);
     log.info("previous profit: "+this.previousProfitPercent);
 
 
-    if(percentage > 0 && !this.open_order) {
-
+    if(percentage > 1 && !this.open_order) {
         log.info("**** Buy: $"+candle.close);
-        this.price = candle.close;
+        this.price = tu.getNorm(candle.close);
         this.open_order = true;
         return this.advice('long');
 
-    } else if(this.open_order && percentage < 0 && currentProfitPercent < this.previousProfitPercent){
-
+    } else if(this.open_order && percentage < 0 ){
         this.open_order = false;
         log.info("**** Sold: $"+candle.close);
         return this.advice('short');
@@ -122,11 +107,8 @@ method.check = function(candle) {
     return this.advice();
 }
 
-method.getCurrentProfitPercent = function(candle) {
-    if(this.price == 0) {
-        return 0;
-    }
-    return ((candle.close - this.price)/this.price)*100;
+method.log = function() {
+
 }
 
 module.exports = method;
